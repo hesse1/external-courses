6d81e6ff5260f3f4d239dd769bf630613d02d5e2
Vadim Tumanov <dope.vadim@mail.ru>
2019-09-26 01:18:55 +0300

    task 6 is done

---
+++ src/ex2_js-basics-part2/task-06.js
@@ -0,0 +1,14 @@
+function checkPrimeNumber(number){
+  if(number >= 1000 || number <= 1){
+     return 'Данные неверны'
+  }
+
+  for(let i = 2; i < number; i++){
+      if(number % i === 0){
+          return('Число' + " " + number + ' - ' + 'составное число');
+      }
+  }
+  return('Число' + " " + number + ' - ' + 'простое число');
+}
+
+module.exports = checkPrimeNumber;
